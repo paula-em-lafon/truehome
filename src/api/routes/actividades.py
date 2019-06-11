927e7d2a2e815b2c24ec2438e297a344c731f570
paula-em-lafon <paula.em.lafon@gmail.com>
2019-06-10 22:54:55 -0600

    aun por probar actividades api y pycache eliminado del gitignore

--- src/api/routes/actividades.py
+++ src/api/routes/actividades.py
@@ -6,46 +6,47 @@ import json
 import ast
 import os
 
-personas_bp = Blueprint('personas', __name__, url_prefix='/api/v1/personas')
+actividades_bp = Blueprint('actividades', __name__,
+                           url_prefix='/api/v1/actividades')
 
 
-@personas_bp.route('/', methods=['GET', 'POST'])
+@actividades_bp.route('/', methods=['GET', 'POST'])
 def create_get_meth():
     args = request.args.to_dict()
     args['api_token'] = os.environ.get('API_TOKEN')
     if request.method == 'POST':
-        response = requests.post(os.environ.get('API_URL') + '/persons',
+        response = requests.post(os.environ.get('API_URL') + '/activities',
                                  params=args, data=request.form)
     if request.method == 'GET':
-        response = requests.get(os.environ.get('API_URL') + '/persons',
+        response = requests.get(os.environ.get('API_URL') + '/activities',
                                 params=args, data=request.form)
     res_dict = response.json()
     return (jsonify(res_dict), response.status_code)
 
 
-@personas_bp.route('/find', methods=['GET'])
-def find_person():
+@actividades_bp.route('/find', methods=['GET'])
+def find_activity():
     args = request.args.to_dict()
     args['api_token'] = os.environ.get('API_TOKEN')
-    response = requests.get(os.environ.get('API_URL') + '/persons/find',
+    response = requests.get(os.environ.get('API_URL') + '/activities/find',
                             params=args, data=request.form)
     res_dict = response.json()
     return (jsonify(res_dict), response.status_code)
 
 
-@personas_bp.route('/<id>', methods=['GET', 'PUT', 'DELETE'])
+@actividades_bp.route('/<id>', methods=['GET', 'PUT', 'DELETE'])
 def delete_get_put_w_id(id):
     pid = str(id)
     args = request.args.to_dict()
     args['api_token'] = os.environ.get('API_TOKEN')
     if request.method == 'GET':
-        response = requests.get(os.environ.get('API_URL') + '/persons/' + pid,
-                                params=args)
+        response = requests.get(os.environ.get('API_URL') + '/activities/' +
+                                pid, params=args)
     if request.method == 'PUT':
-        response = requests.put(os.environ.get('API_URL') + '/persons/' + pid,
-                                params=args, data=request.form)
+        response = requests.put(os.environ.get('API_URL') + '/activities/' +
+                                pid, params=args, data=request.form)
     if request.method == 'DELETE':
-        response = requests.delete(os.environ.get('API_URL') + '/persons/' +
+        response = requests.delete(os.environ.get('API_URL') + '/activities/' +
                                    pid, params=args)
     res_dict = response.json()
     return (jsonify(res_dict), response.status_code)
