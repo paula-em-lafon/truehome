e5f28a7c9225721958513b7e326cf27ece5d18d0
paula-em-lafon <paula.em.lafon@gmail.com>
2019-06-12 10:22:14 -0600

    DEALS WORKS + requirements.txt

--- src/api/routes/deals.py
+++ src/api/routes/deals.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 from flask import request
+from flask import abort
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import json
 import requests
 import os
 import gzip
+
+ALLOWED_EXTENSIONS = set(['pdf'])
 
 deals_bp = Blueprint('deals', __name__, url_prefix='/api/v1/deals')
 
@@ -59,3 +64,41 @@ def list_deal_files(id):
                                 '/files/', params=args)
     res_gzip = gzip.compress(response.content)
     return (res_gzip, response.status_code, response.headers.items())
+
+
+@deals_bp.route('/file', methods=['POST'])
+def upload_deal_file():
+    args = request.args.to_dict()
+    args['api_token'] = os.environ.get('API_TOKEN')
+    if request.method == 'POST':
+        data_err_str = ''
+        data = request.form.to_dict()
+        if not data['deal_id']:
+            data_err_str = 'deal_id not included'
+        elif not set(data.keys()).issubset(['deal_id']):
+            data_err_str = 'no data other than deal_id must be included'
+        if 'file' not in request.files:
+            return abort(400, description="Please select a pdf file " +
+                         data_err_str)
+        file = request.files['file']
+        if file.filename == '':
+            return abort(400, description="Please select a pdf file " +
+                         data_err_str)
+        if file and allowed_file(file.filename):
+            if data_err_str != '':
+                return abort(400, description=data_err_str)
+            m = MultipartEncoder(fields={'deal_id': data['deal_id'],
+                                 'file': (file.filename, file.read(),
+                                          'text/plain')})
+            response = requests.post(os.environ.get('API_URL') + '/files',
+                                     params=args, data=m,
+                                     headers={'Content-Type': m.content_type})
+            res_gzip = gzip.compress(response.content)
+            return (res_gzip, response.status_code, response.headers.items())
+        else:
+            return abort(400, description="File is not pdf" + data_err_str)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
--- src/run.py
+++ src/run.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 from api.routes import personas
 from api.routes import actividades
+from api.routes import deals
+from flask_swagger import swagger
 
 app = Flask(__name__)
 
 app.register_blueprint(personas.personas_bp)
 app.register_blueprint(actividades.actividades_bp)
+app.register_blueprint(deals.deals_bp)
+
+
+@app.route("/api/v1/spec")
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "My API"
+    return jsonify(swag)
 
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0", use_reloader=False)
